118f7410e492a2fede20cbc9e273bc0d33fd46fc
AnnurAfgoni <afgoniannur@gmail.com>
2024-05-27 09:08:46 +0800

    feat: change class names from OnepieceImageDataLoader to OnepieceImageDataModule #3

--- src/lightning_classify/data/__init__.py
+++ src/lightning_classify/data/__init__.py
@@ -1,2 +1,1 @@
 from .datamodule import *
-from .datamodule_v2 import *
--- src/lightning_classify/data/datamodule.py
+++ src/lightning_classify/data/datamodule.py
@@ -5,59 +5,43 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from lightning_classify.transforms import ImageTransform
 
+from lightning import LightningDataModule
 
-class OnepieceImageDataLoader:
-    def __init__(
-            self, 
-            root_path: str, 
-            batch_size: int = 32, 
-            num_workers: int = 11
-        ) -> None:
-        
+
+class OnepieceImageDataModule(LightningDataModule):
+    def __init__(self, root_path: str, batch_size: int = 32, num_workers: int = 11) -> None:
         super().__init__()
         self.root_path = Path(root_path)
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-        self.trainset = self._build_dataset(mode="train")
-        self.validset = self._build_dataset(mode="valid")
+        self.trainset = ImageFolder(
+            root=self.root_path.joinpath("train"),
+            transform=ImageTransform(is_train=True)
+        )
+
+        self.validset = ImageFolder(
+            root=self.root_path.joinpath("val"),
+            transform=ImageTransform(is_train=False)
+        )
 
         self.class_names = self.trainset.classes
         self.class_to_idx = self.trainset.class_to_idx
 
-        self.train_loader = self._build_dataloader(mode="train")
-        self.valid_loader = self._build_dataloader(mode="valid")
-
-    def _build_dataset(self, mode: str = "train") -> ImageFolder:
-        dset = None
-        
-        if mode == "train":
-            trans = ImageTransform(is_train=True)
-            _path = self.root_path.joinpath(mode)
-        elif mode == "valid":
-            trans = ImageTransform(is_train=False)
-            _path = self.root_path.joinpath(mode)
-        else:
-            raise FileNotFoundError("File does not exists.")
-        
-        dset = ImageFolder(str(_path), transform=trans)
-        return dset
-
-    def _build_dataloader(self, mode: str = "train") -> DataLoader:
-        if mode == "train":
-            dset = self.trainset
-            shuffle = True
-        elif mode == "valid":
-            dset = self.validset
-            shuffle = False
-        else:
-            FileNotFoundError("File does not exist.")
-        
+    def train_dataloader(self) -> DataLoader:
         loader = DataLoader(
-            dset, 
+            self.trainset, 
             batch_size=self.batch_size, 
-            shuffle=shuffle, 
-            pin_memory=True
-        )
-
+            num_workers=self.num_workers,
+            shuffle=True
+            )
+        return loader
+    
+    def val_dataloader(self) -> DataLoader:
+        loader = DataLoader(
+            self.validset, 
+            batch_size=self.batch_size, 
+            num_workers=self.num_workers,
+            shuffle=False
+            )
         return loader
