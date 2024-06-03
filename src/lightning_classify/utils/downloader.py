6b83a3a40db32e45214c501a58fa33071d8d7de2
AnnurAfgoni <afgoniannur@gmail.com>
2024-06-03 10:03:41 +0800

    feat: add utils module contains downloader

---
+++ src/lightning_classify/utils/downloader.py
@@ -0,0 +1,15 @@
+import gdown
+from pathlib import Path
+
+
+def downloader(output_file):
+    
+    file_id = "1Ur10cBnPD83uaAXvM_X7tnL4CEdVsUn6"
+    prefix = 'https://drive.google.com/uc?/export=download&id='
+
+    url_download = prefix+file_id
+    
+    if not Path(output_file).exists():
+        print("Downloading...")
+        gdown.download(url_download, output_file)
+        print("Download Finish...")
